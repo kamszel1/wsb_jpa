b12afd733b8a94f019d942c72426fb02c757e8e7
Maciej Dawczak <mac.dawczak@gmail.com>
2025-03-20 19:57:02 +0100

    Add TO classes

---
+++ wsb-jpa1/src/main/java/com/jpacourse/dto/VisitTO.java
@@ -0,0 +1,64 @@
+package com.jpacourse.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisitTO implements Serializable
+{
+    private Long id;
+    private String description;
+    private LocalDateTime time;
+    private String doctorFirstName;
+    private String doctorLastName;
+    private List<String> treatmentTypes = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public String getDoctorFirstName() {
+        return doctorFirstName;
+    }
+
+    public void setDoctorFirstName(String doctorFirstName) {
+        this.doctorFirstName = doctorFirstName;
+    }
+
+    public String getDoctorLastName() {
+        return doctorLastName;
+    }
+
+    public void setDoctorLastName(String doctorLastName) {
+        this.doctorLastName = doctorLastName;
+    }
+
+    public List<String> getTreatmentTypes() {
+        return treatmentTypes;
+    }
+
+    public void setTreatmentTypes(List<String> treatmentTypes) {
+        this.treatmentTypes = treatmentTypes;
+    }
+}
