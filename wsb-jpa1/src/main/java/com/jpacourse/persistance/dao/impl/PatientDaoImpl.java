3a429d5269adb75955c23aee499ff8345aa5ecba
Maciej Dawczak <mac.dawczak@gmail.com>
2025-04-10 20:22:57 +0200

    Add new find methods

--- wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/impl/PatientDaoImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/impl/PatientDaoImpl.java
@@ -1,14 +1,24 @@
 package com.jpacourse.persistance.dao.impl;
 
 import com.jpacourse.persistance.dao.PatientDao;
+import com.jpacourse.persistance.entity.DoctorEntity;
 import com.jpacourse.persistance.entity.PatientEntity;
+import com.jpacourse.persistance.entity.VisitEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public List<PatientEntity> findBySurname(String surname) {
@@ -19,15 +29,46 @@ public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements 
 
     @Override
     public List<PatientEntity> findPatientsWithMoreVisitsThan(int visits) {
-        return entityManager.createQuery("SELECT p FROM PatientEntity p WHERE p.visits.size > :visits", PatientEntity.class)
-                .setParameter("visits", visits)
-                .getResultList();
+        String s = "SELECT p FROM PatientEntity p WHERE SIZE(p.visits) > :visitCount";
+        TypedQuery<PatientEntity> query = entityManager.createQuery(s, PatientEntity.class);
+        query.setParameter("visitCount", visits);
+        return query.getResultList();
+    }
+    @Override
+    public List<PatientEntity> findPatientsBornBefore(LocalDate date) {
+        String s = "SELECT p FROM PatientEntity p WHERE p.dateOfBirth < :date";
+        TypedQuery<PatientEntity> query = entityManager.createQuery(s, PatientEntity.class);
+        query.setParameter("date", date);
+        return query.getResultList();
     }
 
     @Override
-    public List<PatientEntity> findPatientsBornBefore(LocalDate date) {
-        return entityManager.createQuery("SELECT p FROM PatientEntity p WHERE p.dateOfBirth < :date", PatientEntity.class)
-                .setParameter("date", date)
-                .getResultList();
+    public PatientEntity addVisitToPatient(Long patientId, Long doctorId, LocalDateTime visitDate, String description) {
+        PatientEntity patient = entityManager.find(PatientEntity.class, patientId);
+
+        if(patient == null){
+            throw new RuntimeException("Patient not found");
+        }
+        DoctorEntity doctor = entityManager.find(DoctorEntity.class, doctorId);
+
+        if(doctor == null){
+            throw new RuntimeException("Doctor not found");
+        }
+        VisitEntity visit = new VisitEntity();
+        visit.setDoctor(doctor);
+        visit.setTreatments(new ArrayList<>());
+        visit.setPatient(patient);
+        visit.setTime(visitDate);
+        visit.setDescription(description);
+        patient.getVisits().add(visit);
+        return entityManager.merge(patient);
     }
+
+    public List<PatientEntity> findByRegistrationDateAfter(LocalDate date) {
+        TypedQuery<PatientEntity> query = entityManager.createQuery("SELECT p FROM PatientEntity p WHERE p.registrationDate > :date", PatientEntity.class);
+        query.setParameter("date", date);
+        return query.getResultList();
+    }
+
+
 }
